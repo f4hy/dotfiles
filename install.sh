9f090ab6057e94d6cb77f73e8eae767419a27312
Brendan Fahy <bmfahy@gmail.com>
2014-03-06 16:46:34 -0500

    install the ssh config

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Installing homedir dotfiles"
+CURDATE=`date +%Y_%m_%d_%H_%M_%S`
+BACKUPDIR=~/.backup/$CURDATE/
 
-backup(){
-    echo "making backup"
-    CURDATE=`date +%Y_%m_%d_%H_%M_%S`
-    BACKUPDIR=~/.backup/$CURDATE/
+backup_homedir(){
+    echo "making backup of homedir"
     echo "backing up to ${BACKUPDIR}"
     mkdir -p ${BACKUPDIR}
 
@@ -17,7 +17,19 @@ backup(){
     done
 }
 
-install() {
+backup_ssh(){
+    echo "making backup of ssh"
+    echo "backing up to ${BACKUPDIR}ssh_"
+    mkdir -p ${BACKUPDIR}
+    for f in ssh/*; do
+        name=$(basename $f)
+        echo $name
+        ls ~/.ssh/${name}
+        cp ~/.ssh/${name} ${BACKUPDIR}ssh_${name}
+    done
+}
+
+install_homedir() {
     echo "installing"
     for f in homedir/*; do
         name=$(basename $f)
@@ -25,6 +37,16 @@ install() {
         cp -v ${f} ~/.${name}
     done
 }
+
+install_ssh() {
+    echo "installing"
+    for f in ssh/*; do
+        name=$(basename $f)
+        echo "copying ${f} to ~/.ssh/${name}"
+        cp -v ${f} ~/.ssh/${name}
+    done
+}
+
 
 setup_liquidprompt(){
     if [ -d "$HOME/local/liquidprompt/" ]; then
@@ -51,6 +73,8 @@ setup_liquidprompt(){
 }
 
 
-backup
-install
+backup_homedir
+install_homedir
+backup_ssh
+install_ssh
 setup_liquidprompt
